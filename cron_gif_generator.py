f649cba7f3ed91d798085026e2d03c008e5bb37d
Nick Macri <nick.m.macri@gmail.com>
2014-04-15 13:43:12 -0400

    edit submissions first

--- cron_gif_generator.py
+++ cron_gif_generator.py
@@ -2,7 +2,8 @@ import app
 
 post_generator = app.post_generator()
 
+post_generator.edit_all_submissions()
+
 post_generator.generate_photo_posts(count = 20)
 
-post_generator.edit_all_submissions()
 
